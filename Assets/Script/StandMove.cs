670b3ee012d755e589d594d994b50c9f9be780f5
TheComet10 <fedepomini@gmail.com>
2025-03-08 09:24:03 +0100

    Wrist rotation & notation
    Added the wrist (R1) rotation and modified the keys used to rotate the parts

--- Assets/Script/StandMove.cs
+++ Assets/Script/StandMove.cs
@@ -7,13 +7,15 @@ public class StandMove : MonoBehaviour
     [SerializeField] Transform baseUp;
     [SerializeField] Transform b1;
     [SerializeField] Transform b2;
+    [SerializeField] Transform r1;
     [SerializeField] int FPS = 30;
 
-    private bool bW, bC, b1W, b1C, b2W, b2C;
+    private bool bW, bC, b1W, b1C, b2W, b2C, r1W, r1C;
 
     public float vel1 = 1f;
     public float vel2 = 1f;
     public float vel3 = 1f;
+    public float vel4 = 1f;
     void Start()
     {
         Application.targetFrameRate = FPS;
@@ -25,23 +27,30 @@ public class StandMove : MonoBehaviour
         if((Input.GetKey(KeyCode.Q)) || (bW == true))
             baseUp.Rotate(0f, 0f, vel1 * 45f *  Time.deltaTime);
 
-        if ((Input.GetKey(KeyCode.W)) || (bC == true))
+        if ((Input.GetKey(KeyCode.A)) || (bC == true))
             baseUp.Rotate(0f, 0f, vel1 * -45f * Time.deltaTime);
 
         //b1 (205/-19)
-        if (((Input.GetKey(KeyCode.E)) || b1W == true) && ((b1.localRotation.eulerAngles.y <= 205f) || (b1.localRotation.eulerAngles.y > 339f)))
+        if (((Input.GetKey(KeyCode.W)) || b1W == true) && ((b1.localRotation.eulerAngles.y <= 205f) || (b1.localRotation.eulerAngles.y > 339f)))
             b1.Rotate(0f, vel2 * 45f * Time.deltaTime, 0f);
 
-        if (((Input.GetKey(KeyCode.R)) || b1C == true) && ((b1.localRotation.eulerAngles.y < 206f) || (b1.localRotation.eulerAngles.y >= 340f)))
+        if (((Input.GetKey(KeyCode.S)) || b1C == true) && ((b1.localRotation.eulerAngles.y < 206f) || (b1.localRotation.eulerAngles.y >= 340f)))
             b1.Rotate(0f, vel2 * -45f * Time.deltaTime, 0f);
         //b2
-        if ((Input.GetKey(KeyCode.T)) || b2W == true)
+        if ((Input.GetKey(KeyCode.E)) || b2W == true)
             b2.Rotate(0f, vel3 * 45f * Time.deltaTime, 0f);
 
-        if ((Input.GetKey(KeyCode.Y)) || b2C == true)
+        if ((Input.GetKey(KeyCode.D)) || b2C == true)
             b2.Rotate(0f, vel3 * -45f * Time.deltaTime, 0f);
+        //r1
+        if ((Input.GetKey(KeyCode.R))/* || r1W == true*/)
+            r1.Rotate(vel4 * 45f * Time.deltaTime, 0f, 0f);
 
-        if(Input.GetKey(KeyCode.Escape))
+        if ((Input.GetKey(KeyCode.F))/* || r1C == true*/)
+            r1.Rotate(vel4 * -45f * Time.deltaTime, 0f, 0f);
+
+
+        if (Input.GetKey(KeyCode.Escape))
             SceneManager.LoadScene(0);
     }
 
@@ -96,6 +105,23 @@ public class StandMove : MonoBehaviour
     public void B2CcU()
     {
         b2C = false;
+    }
+    public void R1CwD()
+    {
+        r1W = true;
+    }
+    public void R1CwU()
+    {
+        r1W = false;
+    }
+
+    public void R1CcD()
+    {
+        r1C = true;
+    }
+    public void R1CcU()
+    {
+        r1C = false;
     }
 
     public void SliderChange1(float value1)
