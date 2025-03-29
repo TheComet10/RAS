10eb2dca3bdceffdadef83c1e592746f36acce05
TheComet10 <fedepomini@gmail.com>
2025-03-29 11:31:55 +0100

    Added Credits
    I've added the app's creator credits. Also updated the change menu system

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -1,30 +1,51 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
+enum MenuState
+{
+    MAIN,
+    SELECT,
+    CREDITS
+}
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject selMenu;
+    public GameObject credMenu;
+    public GameObject othPan;
+
+    private MenuState state;
 
     private void Start()
     {
+        state = MenuState.MAIN;
+
         mainMenu.SetActive(true);
         selMenu.SetActive(false);
+        credMenu.SetActive(false);
+        othPan.SetActive(true);
+
         Application.targetFrameRate = 30;
     }
 
     private void Update()
     {
-
-        if ((mainMenu.activeSelf == true) && (Input.GetKeyDown(KeyCode.Escape)) && (selMenu.activeSelf == false))
+        if(Input.GetKeyDown(KeyCode.Escape))
         {
-            Application.Quit();
-        }
-        else if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            mainMenu.SetActive(true);
-            
-            selMenu.SetActive(false);
+            if (state == MenuState.SELECT)
+            {
+                Back();
+            }
+            else if (state == MenuState.CREDITS)
+            {
+                Back();
+            }
+            else if(state == MenuState.MAIN)
+            {
+                Application.Quit();
+            }
         }
     }
 
@@ -36,5 +57,31 @@ public class MainMenu : MonoBehaviour
     public void QuitSimu()
     {
         Application.Quit();
+    }
+
+    public void Credits()
+    {
+        mainMenu.SetActive(false);
+        selMenu.SetActive(false);
+        credMenu.SetActive(true);
+        othPan.SetActive(false);
+        state = MenuState.CREDITS;
+    }
+
+    public void Select()
+    {
+        mainMenu.SetActive(false);
+        selMenu.SetActive(true);
+        othPan.SetActive(true);
+        state = MenuState.SELECT;
+    }
+
+    public void Back()
+    {
+        mainMenu.SetActive(true);
+        selMenu.SetActive(false);
+        credMenu.SetActive(false);
+        othPan.SetActive(true);
+        state = MenuState.MAIN;
     }
 }
